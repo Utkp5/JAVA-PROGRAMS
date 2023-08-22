d07d13b1eb46bbbd4f633bb5dad31ce5634104b3
PanditUtkarsh <utkp200@gmail.com>
2023-08-22 18:13:14 +0530

    first commit

---
+++ Star/Filled_ptrn.java
@@ -0,0 +1,25 @@
+package Star;
+
+//Ques   --> below to print
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
+
+
+public class Filled_ptrn {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {   // for row
+            
+            for (int j = 1; j <=5; j++) {  // for column
+                
+                System.out.print(" * ");
+
+            }            
+            System.out.println();
+        }
+    }
+}

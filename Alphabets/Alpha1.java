a472a51e88c02a30969bf11d59635daadadb7f61
PanditUtkarsh <utkp200@gmail.com>
2023-08-22 19:30:27 +0530

    alpha ptrns

---
+++ Alphabets/Alpha1.java
@@ -0,0 +1,25 @@
+package Alphabets;
+
+//Ques   --> below to print
+
+//   A B C D E
+//   A B C D E
+//   A B C D E
+//   A B C D E
+//   A B C D E
+
+public class Alpha1 {
+
+    public static void main(String[] args) {
+        
+        for (int row = 1; row <= 5; row++)   // loop for row
+        {  
+            
+            for (char col = 'A'; col <='E'; col++)   // loop for column
+            {                 
+                System.out.print(col + " ");
+            }            
+            System.out.println();
+        }
+    }
+}

e5fda13e5f9d3246bbf46759d2c3c09770919c30
PanditUtkarsh <utkp200@gmail.com>
2023-08-25 21:43:11 +0530

    ptrns ques

---
+++ Letter/Let1.java
@@ -0,0 +1,30 @@
+package Letter;
+
+//Ques   --> below to print...  its O letter
+
+//    * * * * *
+//    *       *
+//    *       *
+//    *       *
+//    * * * * *  
+
+
+public class Let1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {  
+                
+            for (int j = 1; j <= 5; j++) {
+                
+                if (i == 1 || j == 1 || j == 5 || i == 5) {
+
+                    System.out.print("*");              
+                    
+                }
+                System.out.print(" ");
+            }            
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Letter/Let2.java
@@ -0,0 +1,30 @@
+package Letter;
+
+//Ques   --> below to print...  its O letter
+
+//    * 
+//    *       
+//    *       
+//    *       
+//    * * * * *  
+
+
+public class Let2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {  
+                
+            for (int j = 1; j <= 5; j++) {
+                
+                if (j == 1 || i == 5) {
+
+                    System.out.print("*");              
+                    
+                }
+                System.out.print(" ");
+            }            
+            System.out.println(" ");
+        }
+    }
+}

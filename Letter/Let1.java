3a5e5eb17cd7310d5e8dacbb5ffe842868967ab1
PanditUtkarsh <utkp200@gmail.com>
2023-08-28 21:17:12 +0530

    ptrns ques

--- Letter/Let1.java
+++ Letter/Let1.java
@@ -2,27 +2,23 @@ package Letter;
 
 //Ques   --> below to print...  its O letter   (pending)
 
-//    * * * * *
-//    *       *
-//    *       *   
-//    *       *
-//    * * * * *  
+
 
 
 public class Let1 {
-
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {  
                 
             for (int j = 1; j <= 5; j++) {
                 
-                if (i == 1 || j == 1 || j == 5 || i == 5) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
 
-                    System.out.print("*");              
-                    
+                    System.out.print("*");                    
                 }
-                System.out.print(" ");
+                else {
+                    System.out.print(" ");                    
+                }
             }            
             System.out.println(" ");
         }

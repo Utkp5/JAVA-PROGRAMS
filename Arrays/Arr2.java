3dab7990962b3e94c743b024df1794164676a769
PanditUtkarsh <utkp200@gmail.com>
2023-08-29 20:05:19 +0530

    Arrays  ques

--- Arrays/Arr2.java
+++ Arrays/Arr2.java
@@ -10,6 +10,7 @@ public class Arr2 {
         double marks[] = new double[5];           //Array declaration
 
         double sum = 0, per = 0;
+        int count = 0;
 
         for (int i = 0; i < 5; i++) {    // to get a marks from user 
             
@@ -30,6 +31,31 @@ public class Arr2 {
         }
         System.out.println("Percentage : " + per);
         
+        for (int i = 0; i < 5; i++) {   // for fail subject
+
+            if (marks[i] <=35) {
+                System.out.println("You are fail in " + (i+1) + " No. Subject");
+            }
+        }
+
+        // for (int i = 0; i < 5; i++) {   // for highest marks in subject
+
+        //     if (marks[i] >= marks[i+1]) {
+
+        //         double Hig_marks = marks[i];
+
+        //         System.out.println("You are got Highest marks in " + (i+1) + " Subject");
+        //     }
+        // }
+
+        for (int i = 0; i < 5; i++) {   // for count marks in subject more than 60
+
+            if (marks[i] >= 60) {
+                count++ ;
+            }
+        }
+        System.out.println("You are got more than 60 marks in " + count + " Subject");
+
         
         System.out.println("===================");
         sc.close();
